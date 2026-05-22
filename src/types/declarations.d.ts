3ae93fbbcae600c3dce02e1aa82538e15187caea
Codergoterrors <c0d3r.x0@gmail.com>
2026-05-22 12:37:04 +0530

    fix(driver): add vector-icons type declarations

---
+++ src/types/declarations.d.ts
@@ -0,0 +1,26 @@
+// Global type declarations for modules without @types packages
+
+declare module 'react-native-vector-icons/MaterialCommunityIcons' {
+  import { Icon } from 'react-native-vector-icons/Icon';
+  export default Icon;
+}
+
+declare module 'react-native-vector-icons/MaterialIcons' {
+  import { Icon } from 'react-native-vector-icons/Icon';
+  export default Icon;
+}
+
+declare module 'react-native-vector-icons/Ionicons' {
+  import { Icon } from 'react-native-vector-icons/Icon';
+  export default Icon;
+}
+
+declare module 'react-native-vector-icons/FontAwesome' {
+  import { Icon } from 'react-native-vector-icons/Icon';
+  export default Icon;
+}
+
+declare module 'react-native-vector-icons/FontAwesome5' {
+  import { Icon } from 'react-native-vector-icons/Icon';
+  export default Icon;
+}
