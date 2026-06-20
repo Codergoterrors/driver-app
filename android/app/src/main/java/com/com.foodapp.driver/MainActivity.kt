59cd70f1ef704d0f071846ce72b6284bb2717e08
Codergoterrors <c0d3r.x0@gmail.com>
2026-06-20 20:36:01 +0530

    fix: pass null to super.onCreate to prevent ScreenFragment restore crash on launch

--- android/app/src/main/java/com/com.foodapp.driver/MainActivity.kt
+++ android/app/src/main/java/com/com.foodapp.driver/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.foodapp.driver
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -12,6 +13,18 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "DriverApp"
+
+  /**
+   * Fix for react-native-screens crash:
+   * "Screen fragments should never be restored."
+   * See: https://github.com/software-mansion/react-native-screens/issues/17
+   *
+   * Passing null to super.onCreate() prevents the Android Fragment system from
+   * trying to restore ScreenFragment state, which is incompatible with RN Screens.
+   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
